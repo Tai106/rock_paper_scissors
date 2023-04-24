72a0c17e1e82796a2ab14b9e889dac39e348ed90
Gravity <tevoned@gmail.com>
2023-04-23 23:19:31 -0400

    play again

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 class Game:
     def __init__(self):
         # get the computers choice
-        self.computer_opp = self.get_opp_choice()
+        self.computer_opp = self.get_computer_opp()
 
         # get the user's choice
         self.user_opp = self.get_user_opp()
@@ -17,12 +17,12 @@ class Game:
         random_number = random.randint(1, 3)
 
         # choices
-        option = {1: 'rock', 2: 'paper', 3: 'scissors'}
+        options = {1: 'rock', 2: 'paper', 3: 'scissors'}
 
         # return the value present at radom_number
         return options[random_number]
 
-    def get_user_pick(self):
+    def get_user_opp(self):
 
         while True:
             user_opp = input('Enter rock/paper/scissors: ')
@@ -46,12 +46,30 @@ class Game:
 
         # condition for the user to win
         elif self.user_opp == 'paper' and self.computer_opp == 'rock':
-            return 'win'
+            return 'Win!!!'
         elif self.user_opp == 'rock' and self.computer_opp == 'scissors':
-            return 'win'
+            return 'Win!!!'
         elif self.user_opp == 'scissors' and self.computer_opp == 'paper':
-            return 'win'
+            return 'Win!!!'
 
         # In all other condition user lose
         else:
             return 'lose'
+
+    def print_result(self):
+        print(f"Computer's Choice: {self.computer_opp}")
+        print(f"Your Choice: {self.user_opp}")
+        print(f"You {self.result}")
+
+
+#  created an object of the game class
+while True:
+
+    game = Game()
+    game.print_result()
+
+    play_again = input('Would you like to play again? (y/n): ')
+
+    if play_again != 'y':
+        print('See you soon!')
+        break
